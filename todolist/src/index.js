ae54114c7c8d8283158c9705ed4f3014b3d4fd76
jaesin22 <jaesin22@naver.com>
2021-06-05 00:43:53 +0900

    상단 nav 바 추가

--- todolist/src/index.js
+++ todolist/src/index.js
@@ -1,25 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Button from "@material-ui/core/Button";
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Nav, Navbar, NavDropdown, Form, FormControl } from 'react-bootstrap';
 
 function App() {
   return (
-    <Button variant="contained" color="primary">
+    <Button variant="primary">
       Hello World
     </Button>
   );
 }
 
-function CardHead() {
+function CardHeader() {
   return (
-    <CardHeader>
-      1234
-    </CardHeader>
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-auto" />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+      </Navbar.Collapse>
+    </Navbar>
   );
 
 }
 
 ReactDOM.render(<App />, document.querySelector("#app"));
-ReactDOM.render(<CardHead />, document.querySelector("#cardheader"));
+ReactDOM.render(<CardHeader />, document.querySelector("#cardheader"));
